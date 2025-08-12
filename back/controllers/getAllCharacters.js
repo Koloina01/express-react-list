8a65b78d56c62702ebaec1fef3985675d7012e21
Koloina01 <kolomaherison@gmail.com>
2025-08-12 14:59:43 +0300

    refactor: refactor the structures of the backend

---
+++ back/controllers/getAllCharacters.js
@@ -0,0 +1,27 @@
+import { express } from 'express';
+import { fs } from 'fs';
+import { cors } from 'cors';
+
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/characters', (req, res) => {
+  try {
+    const data = fs.readFileSync('./characters.json', 'utf8'); 
+    const characters = JSON.parse(data);
+    res.json(characters);
+  } catch (err) {
+    console.error('Erreur de lecture du fichier :', err.message);
+    res.status(500).json({ message: 'Erreur serveur' });
+  }
+});
+
+const PORT = 3000;
+
+
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur http://localhost:${PORT}`);
+});
